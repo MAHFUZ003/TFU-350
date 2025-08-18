0d2e47a3d99ea0e8d7de27cacef7793e97002388
MAHFUZ003 <mahfuzy555@gmail.com>
2025-08-18 20:24:44 +0600

    binary tree

---
+++ TFU_DSA/Binary Trees/Traversal/Iterative pre order traversal.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        stack<TreeNode*>q;
+        q.push(root);
+        while(q.size()!=0)
+        {
+            TreeNode* temp = q.top();
+
+            q.pop();
+            if(temp==NULL)
+            {
+                continue;
+            }
+            else
+            {
+                ans.push_back(temp->val);
+            }
+            
+            q.push(temp->right);
+            q.push(temp->left);
+        }
+        return ans;
+    }
+};
